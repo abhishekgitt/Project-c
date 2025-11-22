dcc6077de7b695857307802d316e0d9fd36350b8
Abhishek <abhishekabhi333549@gmail.com>
2025-11-22 13:46:16 +0000

    feat: add Article model

--- news/models.py
+++ news/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    source = models.CharField(max_length=200)
+    title = models.TextField()
+    url = models.URLField(unique=True)
+    published_at = models.DateTimeField(null=True,blank=True)
+    summary = models.TextField(blank=True)
+    fetched_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f"{self.source} - {self.title[:60]}"
